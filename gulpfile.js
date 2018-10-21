66bc3f3cf66f50affcd3123638c1482ef315f424
Akira Kobori <private.beats@gmail.com>
2018-10-22 01:20:40 +0900

    Add a gulp task, sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,8 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass')
+
+gulp.task("sass", function() {
+  return gulp.src("./dist/css/*.css")
+    .pipe(sass())
+    .pipe(gulp.dest("./common/css"));
+});
