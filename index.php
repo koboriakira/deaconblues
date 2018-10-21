8316a3a7b0bb02f2a8f4a76f8fbfb81c4c6993cc
Akira Kobori <private.beats@gmail.com>
2018-10-22 01:30:00 +0900

    import vue.js

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-this is a test.
+<!DOCTYPE html> 
+<title> 
+はじめて の Vue. js
+</title> 
+<script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>
+<div id="app">
+    {{message}}
+</div>
+<script> 
+  console.log(typeof Vue !== 'undefined');
+  new Vue({
+      el: '#app',
+      data: {
+          message: 'Hello!'
+      }
+  });
+</script>
