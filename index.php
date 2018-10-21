8cb7987609747c9a0cdf6b8ac143671e61cbd10b
Akira Kobori <private.beats@gmail.com>
2018-10-22 04:18:51 +0900

    Try to use a vue component.

--- index.php
+++ index.php
@@ -4,14 +4,13 @@
 </title> 
 <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>
 <div id="app">
-    {{message}}
+    <post></post>
 </div>
 <script> 
-  console.log(typeof Vue !== 'undefined');
-  new Vue({
-      el: '#app',
-      data: {
-          message: 'Hello!'
-      }
-  });
+Vue.component('post',{
+    template: '<div>this is a post.</div>'
+})
+new Vue({
+    el: '#app'
+})
 </script>
