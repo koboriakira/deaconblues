56a7da7c3854423700b5a9a793bac5fd58aa81cd
Akira Kobori <private.beats@gmail.com>
2018-10-30 00:57:31 +0900

    Import Bulma

--- index.php
+++ index.php
@@ -1,29 +1,37 @@
 <!DOCTYPE html> 
 <head>
-<title> 
-はじめてのVue.js
-</title>
+<title>はじめてのVue.js</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Vue -->
 <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue-router@3.0.1/dist/vue-router.js"></script>
+
+<!-- axios -->
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+<!-- BULMA -->
+<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+
 </head>
 <body>
 <div id="app">
     <p>コボリアキラの要約と反復</p>
     <li v-for="post in posts" v-bind:key="post.title.rendered">
-        <h1><span v-html="post.title.rendered"></span></h1>
+        <h1 class="title"><span v-html="post.title.rendered"></span></h1>
         <div>
             <span v-html="post.content.rendered"></span>
         </div>
     </li>
     <button
-      class="button is-primary"
-      :class="[{
-        'is-loading': loading,
-        'is-desabled': disabled
-      }]"
-      :disabled="disabled"
-      @click="load"
+        class="button"
+        :class="[{
+            'is-loading': loading,
+            'is-desabled': disabled
+            }]"
+        :disabled="disabled"
+        @click="load"
     >load more</button>
 </div>
 </body>
