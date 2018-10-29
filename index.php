8d948d81ef584505b25a60de116fd4750eca0c6d
Akira Kobori <private.beats@gmail.com>
2018-10-29 23:50:19 +0900

    First vue instance.

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html> 
 <title> 
 はじめてのVue.js
-</title> 
+</title>
 <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/dist/js/test.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue-router@3.0.1/dist/vue-router.js"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <div id="app">
-    <post></post>
+    <h1><span v-html="title"></span></h1>
+    <div v-html="message"></div>
 </div>
-<script> 
-new Vue({
-    el: '#app'
-})
-</script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/common/js/test.js"></script>
