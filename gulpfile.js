97e6ab69bfc7314d8875d534ea89296c528bbeb0
Akira Kobori <private.beats@gmail.com>
2019-02-11 23:25:27 +0900

    :wrench: Add tools for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,11 @@ var pkg = JSON.parse(fs.readFileSync('./package.json'));
 var assetsPath = path.resolve(pkg.path.assetsDir);
 
 var gulp = require('gulp');
-var path = require('path')
-var uglify = require('gulp-uglify')
-var sass = require('gulp-sass')
+var path = require('path');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+const ftp = require('vinyl-ftp');
+const minimist = require('minimist');
 
 gulp.task("js", function () {
   return gulp.src([path.join(assetsPath, 'dist/js/*.js')])
@@ -26,4 +28,36 @@ gulp.task("sass", function () {
 
 gulp.task('default', function () {
   gulp.watch(path.join(assetsPath, 'dist/js/*.js'), ['js']);
+});
+
+gulp.task('ftp', function () {
+  // パスワードを受け取れるようにしたい。-fコマンドが上手くいかん、、、
+
+  // FTPアカウント
+  var conn = ftp.create({
+    host: 'ftp.14451a4e1a4ab9a2.lolipop.jp',
+    user: 'lolipop.jp-14451a4e1a4ab9a2',
+    password: '',
+    parallel: 5,
+  });
+
+  // ローカルのパス
+  var globs = [
+    'assets/**',
+    '*.css',
+    '*.js',
+    '*.php',
+    // 「!」を付けて除外するファイルを指定
+    '!gulpfile.js',
+    '!*.md',
+    '!*.sql',
+  ];
+
+  // ベースディレクトリを「'.'」に記述する
+  return gulp.src(globs, {
+      base: '.',
+      buffer: false
+    })
+    .pipe(conn.newer('wp-content/themes/deaconblues')) // 指定のディレクトリにあるファイルより新しければアップロード
+    .pipe(conn.dest('wp-content/themes/deaconblues')); // 出力するディレクトリ
 });
