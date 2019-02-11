85c040061e2c6f82566227ba504e8e4d3644fb22
Akira Kobori <private.beats@gmail.com>
2019-02-11 23:25:07 +0900

    :fix: Fix blog title

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <head>
-<title>はじめてのVue.js</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>コボリアキラの要約と反復</title>
 
 <!-- Vue -->
 <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>
