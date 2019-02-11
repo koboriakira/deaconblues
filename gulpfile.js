0f9a6650383b120035c3de22633a803f8061696d
Akira Kobori <private.beats@gmail.com>
2019-02-11 20:34:01 +0900

    :bookmark: Use uikit

--- gulpfile.js
+++ gulpfile.js
@@ -8,22 +8,22 @@ var path = require('path')
 var uglify = require('gulp-uglify')
 var sass = require('gulp-sass')
 
-gulp.task("js", function() {
+gulp.task("js", function () {
   gulp.src([path.join(assetsPath, 'dist/js/*.js')])
-  // .pipe(plumber())
-  // .pipe(uglify())
-  // .pipe(rename({extname: '.min.js'}))
-  .pipe(gulp.dest(path.join(assetsPath, 'common/js/')));
+    // .pipe(plumber())
+    // .pipe(uglify())
+    // .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest(path.join(assetsPath, 'common/js/')));
 });
 
-gulp.task("sass", function() {
-  gulp.src([path.join(assetsPath, 'dist/css/*.sass')])
-  .pipe(sass({
-    outputStyle: 'expended'
-  }))
-  .pipe(gulp.dest(path.join(assetsPath, 'common/css/')))
+gulp.task("sass", function () {
+  return gulp.src([path.join(assetsPath, 'dist/css/*.sass')])
+    .pipe(sass({
+      outputStyle: 'expended'
+    }))
+    .pipe(gulp.dest(path.join(assetsPath, 'common/css/')))
 });
 
-gulp.task('default', function() {
-  gulp.watch(path.join(assetsPath, 'dist/js/*.js'),['js']);
+gulp.task('default', function () {
+  gulp.watch(path.join(assetsPath, 'dist/js/*.js'), ['js']);
 });
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <head>
 <title>はじめてのVue.js</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,39 +10,37 @@
 <!-- axios -->
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
-<!-- BULMA -->
-<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+<!-- stylesheet -->
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/common/css/uikit.min.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/common/css/style.css">
 
 </head>
 <body>
 <div id="app">
-    <h1 style="margin: 1em; text-align:center;"><a href="<?php echo home_url(); ?>">コボリアキラの要約と反復</a></h1>
-    <li v-for="post in posts" v-bind:key="post.title.rendered">
-        <section class="hero is-primary">
-            <div class="hero-body">
-                <div class="container">
-                    <h1 class="title" v-html="post.title.rendered" style="font-size: 1.5em;"></h1>
-                </div>
-            </div>
-        </section>
-        <section class="section article">
-            <div class="container">
-                <div v-html="post.content.rendered"></div>
-            </div>
-        </section>
-    </li>
-    <div style="text-align:center;">
-        <button
-            class="button is-info is-medium"
-            :class="[{
-                'is-loading': loading,
-                'is-desabled': disabled
-                }]"
-            :disabled="disabled"
-            v-show="visible"
-            @click="load"
-        >次の記事を読む</button>
+    <header>
+        <h1 class="uk-text-center uk-tile uk-tile-primary"><a href="<?php echo home_url(); ?>">コボリアキラの要約と反復</a></h1>
+    </header>
+    <div class="uk-margin-auto" style="max-width: 800px">
+        <li v-for="post in posts" v-bind:key="post.title.rendered" class="uk-list">
+            <article class="uk-article uk-width-1-1">
+                <h1 class="uk-heading-divider uk-text-center" v-html="post.title.rendered"></h1>
+                <div v-html="post.content.rendered" class="uk-padding-small" style="line-height: 1.8rem;"></div>
+            </article>
+        </li>
+        <div class="uk-text-center">
+            <button
+                class="uk-button"
+                :class="[{
+                    'uk-button-default': loading,
+                    'uk-button-primary': !loading,
+                    'is-disabled': disabled
+                    }]"
+                :disabled="disabled"
+                v-show="visible"
+                @click="load"
+            >{{ textOfLinktNextPost }}</button>
+            <span uk-spinner="ratio: 4.5"></span>
+        <div>
     </div>
     <footer class="footer" style="margin-top: 1em;">
         <div class="content" style="text-align:center;">
@@ -60,7 +58,8 @@ var vm = new Vue({
             page: 0,
             loading: false,
             disabled: false,
-            visible: true
+            visible: true,
+            textOfLinktNextPost: '次の記事を読む'
         }
     },
     mounted: function() {
@@ -72,10 +71,10 @@ var vm = new Vue({
             var array = url.split("/");
             console.log(array.length);
 
-            var URL = `http://localhost:8080/wp-json/wp/v2/posts?page=${this.page}&per_page=1`; 
+            var URL = `http://koboriakira.com/wp-json/wp/v2/posts?page=${this.page}&per_page=1`;
             if (array.length == 7) {
                 var postId = array.slice(-1)[0];
-                URL = `http://localhost:8080/wp-json/wp/v2/posts/${postId}`;
+                URL = `http://koboriakira.com/wp-json/wp/v2/posts/${postId}`;
                 this.visible = false;
             }
             (async () => {
@@ -83,6 +82,7 @@ var vm = new Vue({
                     const res = await axios.get(URL);
                     this.posts = this.posts.concat(res.data);
                     this.loading = false;
+                    this.textOfLinktNextPost = 'Next post';
                 } catch (e) {
                     console.log(e);
                     this.empty();
@@ -93,6 +93,7 @@ var vm = new Vue({
     methods: {
         load() {
             this.loading = true;
+            this.textOfLinktNextPost = 'Now Loading...';
             this.page++;
         },
         empty() {
