dd622c451b6f09f79ef3b6c20ceb2447c50a80c1
Akira Kobori <private.beats@gmail.com>
2019-05-20 00:55:06 +0900

    :horse: Enable to execute APIs in parallel

--- src/assets/common/js/singleton/InitSingleton.js
+++ src/assets/common/js/singleton/InitSingleton.js
@@ -2,7 +2,5 @@ import Categories from "./categories";
 import Tags from "./tags";
 
 export default async function () {
-  for (const singleton of [Categories, Tags]) {
-    await singleton.init();
-  }
+  await Promise.all([Categories.init(), Tags.init()]);
 }
